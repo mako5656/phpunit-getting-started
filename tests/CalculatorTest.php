62072078841a380a797c2d53d0d505203af135e2
Makoto Ikami <makoto.ikami@gmail.com>
2022-12-22 21:47:30 +0900

    テストを落としてみる

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -9,7 +9,7 @@ class CalculatorTest extends TestCase
     public function test_合計金額()
     {
         $calculator = new Calculator();
-        $this->assertSame(12, $calculator->add(10, 2));
+        $this->assertSame(10, $calculator->add(10, 2));
     }
 
     public function test_最初の金額から次の金額を引いた金額()
